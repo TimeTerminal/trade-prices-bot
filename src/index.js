ce349f255b4ed18637506c1ade8c72f1494333ca
Sam Hogg <sam@samhogg.com>
2018-07-27 13:34:43 -0400

    fix object

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ const slackbot = require("slackbots");
 const axios = require("axios");
 
 const bot = new Slackbot({
-  token: 'xoxb-4886555281-406059636004-9D2cKYQOGDnU9pZpz4teVdmJ';
-  name: 'tradebot';
+  token: "xoxb-4886555281-406059636004-9D2cKYQOGDnU9pZpz4teVdmJ",
+  name: "tradebot"
 });
