77d71740bb03d225ddb17d5e6641b6e755ae8414
Vishesh <vishesh.thanki@gmail.com>
2018-07-27 14:01:07 -0400

    Slackbot initialization

--- src/index.js
+++
@@ -1,7 +0,0 @@
-const slackbot = require("slackbots");
-const axios = require("axios");
-
-const bot = new Slackbot({
-  token: "xoxb-4886555281-406059636004-9D2cKYQOGDnU9pZpz4teVdmJ",
-  name: "tradebot"
-});
