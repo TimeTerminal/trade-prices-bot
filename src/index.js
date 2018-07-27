6b932db9aa34f1a92b2629aa09fed04bbc4cac20
Vishesh <vishesh.thanki@gmail.com>
2018-07-27 13:17:12 -0400

    Add Axios and slackbots packages

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const slackbot = require("slackbots");
+const axios = require("axios");
+
+const bot = new Slackbot({
+  token: 'xoxb-4886555281-406059636004-9D2cKYQOGDnU9pZpz4teVdmJ';
+  name: 'tradebot';
+});
